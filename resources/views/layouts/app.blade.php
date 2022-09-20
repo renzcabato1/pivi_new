06477b553e218d07c6b3fdb80d0fbd3f10b16b1d
RDMontana06 <montanareymart06@gmail.com>
2022-09-20 16:43:23 +0800

    Fix Viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     
         <title>{{ config('app.name', 'Laravel') }}</title>
         <meta content="Holdings, Construction Company, Shipping, Logistics, Ship Building" name="keywords">
